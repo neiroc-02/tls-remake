b80b69d957a3d5f066bb6b5a37c060c543262b10
neiroc-02 <neiro.cabrera@yahoo.com>
2024-11-21 21:06:29 -0800

    Added helper function

--- project/consts.h
+++ project/consts.h
@@ -140,3 +140,31 @@ static inline void print_hex(uint8_t* buf, size_t len) {
     }
     fprintf(stderr, "\n");
 }
+
+/* Helper function to print buffers*/
+#define MIN(a, b) (a < b) ? a : b
+static inline void print_tlv(uint8_t* buffer, size_t len) {
+    uint8_t* buf = buffer;
+
+    while (buf - buffer < len) {
+        uint8_t type = *buf;
+        fprintf(stderr, "Type: 0x%02x\n", type);
+        buf += 1;
+        if (buf - buffer >= len)
+            return;
+
+        uint16_t length = ntohs(*((uint16_t*) buf));
+        buf += 2;
+        fprintf(stderr, "Length: %hu\n", length);
+
+        if (type == CLIENT_HELLO || type == SERVER_HELLO ||
+            type == KEY_EXCHANGE_REQUEST || type == FINISHED ||
+            type == CERTIFICATE || type == DATA)
+            continue;
+        else {
+            uint16_t min_length = MIN(len - (buf - buffer), length);
+            print_hex(buf, min_length);
+            buf += min_length;
+        }
+    }
+}
